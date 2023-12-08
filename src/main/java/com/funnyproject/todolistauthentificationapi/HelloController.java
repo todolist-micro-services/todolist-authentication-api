2169241cff699ea0f4838fefabcb689fc2487a89
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-12-07 22:53:51 -0800

    feat(jwt): add library for jwt token

--- src/main/java/com/funnyproject/todolistauthentificationapi/HelloController.java
+++ src/main/java/com/funnyproject/todolistauthentificationapi/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import todolist.database.DataInterface;
 import todolist.database.mysql.Mysql;
+import todolist.jwttoken.JwtToken;
+import todolist.jwttoken.JwtTokenType;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -28,8 +30,15 @@ public class HelloController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        DataInterface dataInterface = new Mysql(System.getProperty("DB_URL"), System.getProperty("DB_USERNAME"), System.getProperty("DB_PASSWORD"));
-        return String.valueOf(dataInterface.getUser("newemail@example.com", "newpassword"));
+//        DataInterface dataInterface = new Mysql(System.getProperty("DB_URL"), System.getProperty("DB_USERNAME"), System.getProperty("DB_PASSWORD"));
+//        return String.valueOf(dataInterface.getUser("newemail@example.com", "newpassword"));
+        String secret = "asdfSFS34wfsdfsdfSDSD32dfsddDDerQSNCK34SOWEK5354fdgdf4";
+        String name = "John Doe";
+        String email = "johndoe@gmail.com";
+        String subject = "John";
+        int nbrHour = 24;
+        JwtTokenType token = JwtToken.createJwtToken(secret, name, email, subject, nbrHour);
+        return token.getJwtValue();
     }
 
 }
