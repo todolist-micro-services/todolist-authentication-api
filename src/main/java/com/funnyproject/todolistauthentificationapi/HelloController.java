0f330a76fb0b5fe4619a02be235f4024bc1a5fb5
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-12-07 16:17:27 -0800

    fix(env): change env var system

--- src/main/java/com/funnyproject/todolistauthentificationapi/HelloController.java
+++ src/main/java/com/funnyproject/todolistauthentificationapi/HelloController.java
@@ -6,21 +6,29 @@ import org.springframework.web.bind.annotation.RestController;
 import todolist.database.DataInterface;
 import todolist.database.mysql.Mysql;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 @RestController
 public class HelloController {
 
-    @Value("${DB_URL}")
-    private String databaseUrl;
-
-    @Value("${DB_USERNAME}")
-    private String databaseUser;
-
-    @Value("${DB_PASSWORD}")
-    private String databasePassword;
-
     @GetMapping("/")
     public String index() {
-        DataInterface dataInterface = new Mysql(databaseUrl, databaseUser, databasePassword);
+        try (BufferedReader reader = new BufferedReader(new FileReader("/Users/pad/delivery/todoList/todolist-authentification-api/.env"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("=");
+                if (System.getProperties().containsKey(parts[0])) {
+                    System.getProperties().setProperty(parts[0], parts[1]);
+                } else {
+                    System.setProperty(parts[0], parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        DataInterface dataInterface = new Mysql(System.getProperty("DB_URL"), System.getProperty("DB_USERNAME"), System.getProperty("DB_PASSWORD"));
         return String.valueOf(dataInterface.getUser("newemail@example.com", "newpassword"));
     }
 
--- src/main/java/com/funnyproject/todolistauthentificationapi/TodolistApiApplication.java
+++ src/main/java/com/funnyproject/todolistauthentificationapi/TodolistApiApplication.java
@@ -3,14 +3,30 @@ package com.funnyproject.todolistauthentificationapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collections;
 
 @SpringBootApplication
 public class TodolistApiApplication {
 
 	public static void main(String[] args) {
+		try (BufferedReader reader = new BufferedReader(new FileReader("/Users/pad/delivery/todoList/todolist-authentification-api/.env"))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] parts = line.split("=");
+				if (System.getProperties().containsKey(parts[0])) {
+					System.getProperties().setProperty(parts[0], parts[1]);
+				} else {
+					System.setProperty(parts[0], parts[1]);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		SpringApplication app = new SpringApplication(TodolistApiApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+		app.setDefaultProperties(Collections.singletonMap("server.port", System.getProperty("SERVER_PORT")));
 
 		app.run(args);
 	}
